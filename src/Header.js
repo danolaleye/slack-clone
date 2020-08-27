93413699a29ac17eb7d0bd79e155a1635bbd5d6a
Dan Olaleye <danolaleye@gmail.com>
2020-08-26 20:02:14 -0400

    Initial Commit

---
+++ src/Header.js
@@ -0,0 +1,27 @@
+import React from "react";
+import "./Header.css";
+import { Avatar } from "@material-ui/core";
+import AccessTimeIcon from "@material-ui/icons/AccessTime";
+import SearchIcon from "@material-ui/icons/Search";
+import HelpOutlineIcon from "@material-ui/icons/HelpOutline";
+
+function Header() {
+  return (
+    <div className="header">
+      <div className="header__left">
+        <Avatar className="header__avatar" alt="Dan" src />
+        <AccessTimeIcon />
+      </div>
+
+      <div className="header__search">
+        <SearchIcon />
+        <input placeholder="Search Outline Icon" />
+      </div>
+      <div className="header__right">
+        <HelpOutlineIcon />
+      </div>
+    </div>
+  );
+}
+
+export default Header;
